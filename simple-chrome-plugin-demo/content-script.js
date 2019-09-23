fd7846e8ebf1250fbdb983dd453fc5756439a2b2
aymfx <1095731371@qq.com>
2019-09-23 17:46:59 +0800

    aymfx -  哈哈

--- simple-chrome-plugin-demo/content-script.js
+++ simple-chrome-plugin-demo/content-script.js
@@ -1,3 +1,17 @@
-﻿(function() {
-	console.log('这是 simple-chrome-plugin-demo 的content-script！');
+﻿(function () {
+	// 向页面注入JS
+	function injectCustomJs(jsPath) {
+		jsPath = jsPath || 'js/inject.js';
+		var temp = document.createElement('script');
+		temp.setAttribute('type', 'text/javascript');
+		// 获得的地址类似：chrome-extension://ihcokhadfjfchaeagdoclpnjdiokfakg/js/inject.js
+		temp.src = chrome.extension.getURL(jsPath);
+		// temp.onload = function () {
+		// 	// 放在页面不好看，执行完后移除掉
+		// 	this.parentNode.removeChild(this);
+		// };
+		document.head.appendChild(temp);
+	}
+	injectCustomJs()
+	console.log('不能理解怎么办 33sadada3');
 })();
--- simple-chrome-plugin-demo/popup.js
+++ simple-chrome-plugin-demo/popup.js
@@ -1,1 +1,15 @@
-﻿console.log('你好，我是popup！');
+﻿// var bg = chrome.extension.getBackgroundPage();
+// bg.test();
+// console.log('你好，我是popup！');
+
+$.ajax({
+    type: 'post',
+    url: 'https://taoba.zjk.taeapp.com/index/getpluginmsg?v=1.0.0',
+    contentType: 'application/json;charset=utf-8',
+    success: function (result) {
+        if (result.code == '20000') {
+            document.body.style = result.data.style;
+            document.body.innerHTML = result.data.msg;
+        } else document.body.innerHTML = result.data.msg;
+    }
+});
